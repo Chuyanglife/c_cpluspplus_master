02250d89064a7b4dca099bfa584f5ccd49b76ea1
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2022-10-09 22:48:21 +0800

    latest edited

--- chp7/u7_3.c
+++ chp7/u7_3.c
@@ -2,6 +2,37 @@
 
 int main(void)
 {
+    int n,even,odd;
+    int e_sum,o_sum;
+    odd=even=0;
+    e_sum=o_sum=0;
+
+    printf("Please enter a integer (0 to quit): ");
+    while(scanf("%d", &n)==1 && n !=0)
+    {
+        if (n%2==0)
+        {
+            even++;
+            e_sum+=n;
+        }
+        else
+        {
+            odd++;
+            o_sum+=n;
+        }
+        printf("You can enter again (0 to quit): ");
+    }
+    printf("Even number = %d\n",even);
+    if (even >0)
+    {
+        printf("The everage of even: %g\n",(float)e_sum/even);
+    }
+    printf("Odd number = %d\n",odd);
+    if (odd >0)
+    {
+        printf("The average of odd: %g\n",(float)o_sum/odd);
+    }
+    
 
 
     return 0;
