bcb435b1057875458c4bbf8a28b84cce358939df
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2022-10-12 01:09:36 +0800

    edited

--- demo/demo_4.c
+++ demo/demo_4.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
-
-int main(void)
+#include "stdio.h"
+int main()
 {
-    long num;
-    long sum = 0L;
-    int status;
-
-    
-
-    return 0;
-}
+    for (size_t i = 0; i < 10; i++) //控制打印行数(一行为一个循环)
+    {
+         for (size_t j = 1; j <= i; j++) //控制打印列数
+        {
+            printf("  ");//两个空格时是直角三角形，一个空格是等腰三角形
+        }
+        for (size_t k = 10; k>i; k--) //控制打印列数
+        {
+            printf("?");
+        }
+        printf("\n"); //每一次列循环后另起行
+    }
+} 
