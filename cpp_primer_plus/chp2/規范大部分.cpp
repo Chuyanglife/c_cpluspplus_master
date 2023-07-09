2dc772fdb71431712ea54c7e1da67d57b60bd693
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-07-09 11:13:33 +0800

    ok

--- cpp_primer_plus/chp2/規范大部分.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"Wong Yao Hong"<<endl;
-    cout<<"new taipei city"<<endl;
-    cout<<"好煩好煩共和國"<<endl;
-    system("pause");
-    return 0;
-}
