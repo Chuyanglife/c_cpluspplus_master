6a3db4fbd031365be6e5c26184d522696110de1e
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-07-15 10:21:07 +0800

    ok

--- cpp_primer_plus/demo_plus/demo1.cpp
+++ cpp_primer_plus/demo_plus/demo1.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"mapukila dad"<<endl;
+    cout<<"mapukila dad";
+    cin.get();
+    cout<<"mapukila mom"<<endl;
     cin.get();
     //system("pause");
     return 0;
