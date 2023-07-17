f2f149e2e1af38e1e7585caf256b709bfc5e0bda
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-07-17 11:01:48 +0800

    vault backup: 2023-07-17 11:01:48

--- cpp_primer_plus/chp2/u2_7.cpp
+++ cpp_primer_plus/chp2/u2_7.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 using namespace std;
-
+void print_time(int h, int m){
+    cout<<"Time: "<<h<<":"<<m<<endl;
+}
 int main(){
-    
+    cout<<"Enter the number of hours: ";
+    int hours;
+    cin>>hours;
+    cout<<"Enter the number of minutes: ";
+    int minutes;
+    cin>>minutes;
+    print_time(hours,minutes);
     system("pause");
     return 0;
 }
