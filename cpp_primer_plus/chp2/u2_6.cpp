f7bdd608cbd0ac84c5814f3254ce5e7e690d9e03
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-07-17 10:32:24 +0800

    vault backup: 2023-07-17 10:32:24

--- cpp_primer_plus/chp2/u2_6.cpp
+++ cpp_primer_plus/chp2/u2_6.cpp
@@ -3,7 +3,8 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    cout<<"Enter the number of light years"
+    cout<<"Enter the number of light years: "<<endl;
+    int 
     system("pause");
     return 0;
 }
