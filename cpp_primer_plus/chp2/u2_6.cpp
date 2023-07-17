9a58ced57e42d1953740fc025f55f2ea8a2efa77
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-07-17 10:36:48 +0800

    vault backup: 2023-07-17 10:36:48

--- cpp_primer_plus/chp2/u2_6.cpp
+++ cpp_primer_plus/chp2/u2_6.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    cout<<"Enter the number of light years: "<<endl;
-    int light_year;
-    cin>>light_year;
+double convert(double lightyear){
     
+}
+int main(){
+    cout<<"Enter the number of light years: "<<endl;
+    double light_year;
+    cin>>light_year;
+    double astronimical_units=light_year;
+    cout<<light_year<<" light years = "<<astronimical_units<<" astronimical units."<<endl;
     system("pause");
     return 0;
 }
