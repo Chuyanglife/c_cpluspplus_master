91d95f31f5b46e299b3e440dc5cb801d1a2c7ffa
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-07-20 11:24:42 +0800

    vault backup: 2023-07-20 11:24:42

--- cpp_primer_plus/chp3/u3_2.cpp
+++ cpp_primer_plus/chp3/u3_2.cpp
@@ -13,6 +13,7 @@ int main(){
     int height_feet,height_inches;
     double height_meters;
     const int feet_per_inches=12;
+    const double 
     height_feet=feet*feet_per_inches;
     height_meters=height_feet+inches;
     cout<<"That means you are "<<height_meters<<" meters height and "<<" kilograms weight."<<endl;
