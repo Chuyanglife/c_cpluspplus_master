6c7490df19a7496cd23f2081e32dcb221ac98e8b
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-07-20 11:19:45 +0800

    vault backup: 2023-07-20 11:19:45

--- cpp_primer_plus/chp3/u3_2.cpp
+++ cpp_primer_plus/chp3/u3_2.cpp
@@ -11,7 +11,8 @@ int main(){
     cout<<"So you are "<<feet<<" feet and "<<inches<<" inches height and "<<weight<<" pounds weight."<<endl;
     
     int height_feet,height_inches,height_meters;
-    height_meters=
+    const int 
+    height_feet=feet*12;
     cout<<"That means you are "<<" meters height and "<<" kilograms weight."<<endl;
     cout<<"And your BMI is "<<endl;
     system("pause");
