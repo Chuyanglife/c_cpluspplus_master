989e2baae4be4a173135d6b6be4ebe8406bc8dcd
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-07-20 10:59:29 +0800

    vault backup: 2023-07-20 10:59:29

--- cpp_primer_plus/chp3/u3_1.cpp
+++ cpp_primer_plus/chp3/u3_1.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    cout<<"Enter your height: ";
+    int height;
+    cin>>height;
+    int feet,inches;
+    const int inches_per_height =12;
+    feet = height/inches_per_height;
+    inches = height%inches_per_height;
+    cout<<"That means you are "<<feet<<" feet and "<<inches<<" inches height."<<endl;
     system("pause");
     return 0;
 }
