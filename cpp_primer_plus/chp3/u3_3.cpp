e9548e9561623bb17de205a8a888a9d3d146c379
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-07-26 20:11:21 +0800

    vault backup: 2023-07-26 20:11:21

--- cpp_primer_plus/chp3/u3_3.cpp
+++ cpp_primer_plus/chp3/u3_3.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int deg,min,sec;
+    double final_deg=0;
     cout<<"Enter a latitude in degrees, minutes, and seconds:"<<endl;
+    cout<<"First, enter the degrees: ";
+    cin>>deg;
+    cout<<"Next, enter the minutes of arc: ";
+    cin>>min;
+    cout<<"Finally, enter the seconds of arc: ";
+    cin>>sec;
+
+    cout<<deg<<" degrees, "<<min<<" minutes, "<<sec<<" seconds = "<<final_deg<<" degrees"<<endl;
+    system("pause");
     return 0;
 }
