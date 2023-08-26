c9f6478456a80101fd382d10a6a44012f290003d
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-08-26 22:06:23 +0800

    vault backup: 2023-08-26 22:06:23

--- cpp_primer_plus/chp3/u3_4.cpp
+++ cpp_primer_plus/chp3/u3_4.cpp
@@ -4,6 +4,7 @@ int main(){
     long sec;
     cout<<"Enter the number of seconds: ";
     cin>>sec;
+    const int 
     cout<<sec<<" seconds = "<<" days,"<<" hours,"<<" minutes,"<<" seconds"<<endl;
     system("pause");
     return 0;
