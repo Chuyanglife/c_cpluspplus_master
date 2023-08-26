9a14f39b3d69dda74a970cbbd56ad04c6d8285f0
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-08-26 15:55:53 +0800

    ok

--- cpp_primer_plus/chp3/u3_4.cpp
+++ cpp_primer_plus/chp3/u3_4.cpp
@@ -1,12 +1,36 @@
-#include<iostream>
+/* #include<iostream>
 using namespace std;
 int main(){
-    long long sec;
-    long d,h,m,s;
+    long sec;
     cout<<"Enter the number of seconds: ";
     cin>>sec;
-    const long int i;
     cout<<sec<<" seconds = "<<" days,"<<" hours,"<<" minutes,"<<" seconds"<<endl;
+    system("pause");
+    return 0;
+} */
+// 3.4.cpp: 定义控制台应用程序的入口点。
+// 3.4.cpp: 显示时间单位转换
+
+#include <iostream>
+ 
+int main()
+{
+    using namespace std;
+    cout << "Enter the number of seconds:________\b\b\b\b\b\b\b\b";
+    long sec;
+    cin >> sec;
+    const int Min_per_sec = 60;
+    const int H_per_min = 60;
+    const int D_per_h = 24;
+    int s, d, h, min;
+    s = sec % Min_per_sec;
+    int convert;
+    convert = sec / Min_per_sec;
+    min = convert % H_per_min;
+    convert = convert / H_per_min;
+    h = convert % D_per_h;
+    d = convert / D_per_h;
+    cout << sec << " seconds = " << d << " days, " << h << " hours, " << min << " minutes, " << s << " seconds" << endl;
     system("pause");
     return 0;
 }
--- cpp_primer_plus/demo_plus/demo1.cpp
+++ cpp_primer_plus/demo_plus/demo1.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/* #include<iostream>
 using namespace std;
 int main(){
     cout<<"mapukila dad";
@@ -6,5 +6,19 @@ int main(){
     cout<<"mapukila mom"<<endl;
     cin.get();
     //system("pause");
+    return 0;
+} */
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    const int aa=60;
+    const int bb=60;
+    const int cc=24;
+    int a1,b1,c1,tmp=0;
+    a1=a%aa;
+    cout<<a1<<endl;
+    system("pause");
     return 0;
 }
