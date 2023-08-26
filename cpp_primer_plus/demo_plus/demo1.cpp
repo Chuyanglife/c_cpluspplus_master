2f84f6e5ff02039295534900a6933b40119e9b1a
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-08-26 22:23:09 +0800

    vault backup: 2023-08-26 22:23:09

--- cpp_primer_plus/demo_plus/demo1.cpp
+++ cpp_primer_plus/demo_plus/demo1.cpp
@@ -8,17 +8,3 @@ int main(){
     //system("pause");
     return 0;
 } */
-#include<iostream>
-using namespace std;
-int main(){
-    int a;
-    cin>>a;
-    const int aa=60;
-    const int bb=60;
-    const int cc=24;
-    int a1,b1,c1,tmp=0;
-    a1=a%aa;
-    cout<<a1<<endl;
-    system("pause");
-    return 0;
-}
