a29af27ad6c7d1977dba296be9d68fd744d6d346
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-08-26 22:03:26 +0800

    vault backup: 2023-08-26 22:03:26

--- cpp_primer_plus/chp3/u3_4.cpp
+++ cpp_primer_plus/chp3/u3_4.cpp
@@ -1,4 +1,4 @@
-/* #include<iostream>
+#include<iostream>
 using namespace std;
 int main(){
     long sec;
@@ -7,28 +7,5 @@ int main(){
     cout<<sec<<" seconds = "<<" days,"<<" hours,"<<" minutes,"<<" seconds"<<endl;
     system("pause");
     return 0;
-} */
-
-#include <iostream>
-
-int main()
-{
-    using namespace std;
-    cout << "Enter the number of seconds:________\b\b\b\b\b\b\b\b";
-    long sec;
-    cin >> sec;
-    const int Min_per_sec = 60;
-    const int H_per_min = 60;
-    const int D_per_h = 24;
-    int s, d, h, min;
-    s = sec % Min_per_sec;
-    int convert;
-    convert = sec / Min_per_sec;
-    min = convert % H_per_min;
-    convert = convert / H_per_min;
-    h = convert % D_per_h;
-    d = convert / D_per_h;
-    cout << sec << " seconds = " << d << " days, " << h << " hours, " << min << " minutes, " << s << " seconds" << endl;
-    system("pause");
-    return 0;
 }
+
