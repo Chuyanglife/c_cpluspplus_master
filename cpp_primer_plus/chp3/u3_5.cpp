7b0f1f26ca8cd1201424ebc7a6b56c12bdb0a380
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-08-27 15:52:37 +0800

    vault backup: 2023-08-27 15:52:37

--- cpp_primer_plus/chp3/u3_5.cpp
+++ cpp_primer_plus/chp3/u3_5.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Running"<<endl;
+    long long world,US;
+    cout<<"Enter the world's population: ";
+    cin>>world;
+    cout<<"Enter the population of the US: ";
+    cin>>US;
+    const int percent = 100;
+    float convert = float(US)/float(world)*percent;
+    cout<<"The population of the US is "<<convert<<"% of the world population."<<endl;
+    
     system("pause");
     return 0;
 }
