4acd682296532047f594818a38e1d947911b22ba
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-09-04 12:11:37 +0800

    vault backup: 2023-09-04 12:11:37

--- cpp_primer_plus/chp3/u3_6.cpp
+++ cpp_primer_plus/chp3/u3_6.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"Hello world!"<<endl;
+    long long miles,calons;
+    cout<<"Enter your miles:";
+    cin>>miles;
+    cout<<"Enter your calons:";
+    cin>>calons;
+    float rate=(float)miles/(float)calons;
+    cout<<"You drive "<<rate<<" miles using"<<" 1 calon gas"<<endl;
     system("pause");
     return 0;
 }
