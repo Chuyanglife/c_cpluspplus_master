f6adf78c9c9ce24dae60d4800e0da52037c28f49
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-09-04 12:35:41 +0800

    vault backup: 2023-09-04 12:35:41

--- cpp_primer_plus/chp3/u3_7.cpp
+++ cpp_primer_plus/chp3/u3_7.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     cout<<"Enter your kilometers:";
     cout<<"Enter your litros:";
-    cout<<"Every "<<"kilo"
+    cout<<"Every "<<"kilometers cost "
     system("pause");
     return 0;
 }
