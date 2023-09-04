150e826008b364c2612a2a4297428458128a4fe1
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-09-04 12:46:30 +0800

    vault backup: 2023-09-04 12:46:30

--- cpp_primer_plus/chp3/u3_7.cpp
+++ cpp_primer_plus/chp3/u3_7.cpp
@@ -8,7 +8,7 @@ int main(){
     cout<<"Enter your litros:";
     double litros;
     cin>>litros;
-    cout<<
+    cout<<setprecision(10);
     cout<<"Every "<<kilometers<<" kilometers cost "<<litros<<" litros gas in Europe."<<endl;
     cout<<"That means you drive "<<"miles using 1 calons gas in the US."<<endl;
     system("pause");
