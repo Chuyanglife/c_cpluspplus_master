a6df62a557d35cab15a37e6cb7fd74f326d97a20
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-09-04 12:12:20 +0800

    vault backup: 2023-09-04 12:12:20

--- cpp_primer_plus/demo_plus/demo1.cpp
+++ cpp_primer_plus/demo_plus/demo1.cpp
@@ -1,10 +1,13 @@
-/* #include<iostream>
+#include<iostream>
 using namespace std;
 int main(){
     cout<<"mapukila dad";
     cin.get();
     cout<<"mapukila mom"<<endl;
     cin.get();
+    int a,b,c;
+    cin>>a>>b;
+    c=a/b
     //system("pause");
     return 0;
-} */
+} 
