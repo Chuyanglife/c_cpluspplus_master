4ec17266039b86c20f517bcbb880b1d78f53c7c5
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-03 19:39:55 +0800

    vault backup: 2023-10-03 19:39:55

--- cpp_primer_plus/chp4/u4_2.cpp
+++ cpp_primer_plus/chp4/u4_2.cpp
@@ -4,7 +4,10 @@ int main(){
     string name;
     string dessert;
     cout<<"Enter your name:"<<endl;
+    getline(cin,name);
     cout << "Enter your favorite dessert:"<<endl;
-
+    getline(cin,dessert);
+    cout<<"I have delicious "<<dessert;
+    cout<<"for you, "<<name<<"."<<endl;
     return 0;
 }
