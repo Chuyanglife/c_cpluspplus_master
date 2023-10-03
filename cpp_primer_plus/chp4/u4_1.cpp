1b3fc23687ca48004daddfa8b1a65a9381d948cd
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-03 16:52:56 +0800

    vault backup: 2023-10-03 16:52:55

--- cpp_primer_plus/chp4/u4_1.cpp
+++ cpp_primer_plus/chp4/u4_1.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-    char first_name[100];
-    char last_name[100];
+    char firstname[100];
+    char lastname[100];
     char grade;
     int age;
     cout<<"What is your first name?";
-    cin.getline(first_name,100);
+    cin.getline(firstname,100);
     cout<<"What is your last name?";
-    cin.getline(first_name,80);
+    cin.getline(lastname,80);
     cout<<"What letter grade do you deserve?";
     cin>>grade;
     cout<<"What is your age?";
     cin>>age;
-    cout<<"Name:"<<first_name<<last_name<<endl;
+    cout<<"Name:"<<lastname<<","<<firstname<<endl;
     cout<<"Grade:"<<grade<<endl;
     cout<<"Age:"<<age<<endl;
     return 0;
