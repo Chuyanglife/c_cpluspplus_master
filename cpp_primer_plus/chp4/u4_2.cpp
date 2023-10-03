4aa1e13537a077be463db95e03375c49f6db74c3
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-03 19:38:04 +0800

    vault backup: 2023-10-03 19:38:04

--- cpp_primer_plus/chp4/u4_2.cpp
+++ cpp_primer_plus/chp4/u4_2.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
+    string name;
+    string dessert;
     cout<<"Enter your name:"<<endl;
     cout << "Enter your favorite dessert:"<<endl;
-    
+
     return 0;
 }
