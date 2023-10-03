807c63fa53e3e5465821ed4f72b3c800e7a08835
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-03 16:17:56 +0800

    vault backup: 2023-10-03 16:17:56

--- cpp_primer_plus/chp4/u4_1.cpp
+++ cpp_primer_plus/chp4/u4_1.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 int main(){
     cout<<"What is your first name?";
+    cout<<"What is your last name?";
+    cout<<"What letter grade do you deserve?";
+    cout<<"What is your age?";
+    cout<<"Name:";
+    cout<<"Grade:";
+    cout<<"Age:";
     return 0;
 }
