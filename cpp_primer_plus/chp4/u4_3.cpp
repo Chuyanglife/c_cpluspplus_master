0e6e7f6bed918cb0a4391dd5885db89383100e80
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-07 23:15:04 +0800

    vault backup: 2023-10-07 23:15:04

--- cpp_primer_plus/chp4/u4_3.cpp
+++ cpp_primer_plus/chp4/u4_3.cpp
@@ -2,7 +2,7 @@
 #include<cstring>
 using namespace std;
 int main(){
-    char firstname[20],
+    char firstname[20], lastname[20];
     cout<<"Enter your first name: ";
     cout<<"Enter your last name: ";
     cout<<"Here's the information in a single string: ";
