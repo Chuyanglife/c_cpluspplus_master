8b5ecf15058c144f66c0a707972374297bb1d12b
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-07 23:13:16 +0800

    vault backup: 2023-10-07 23:13:16

--- cpp_primer_plus/chp4/u4_3.cpp
+++ cpp_primer_plus/chp4/u4_3.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main(){
     cout<<"Enter your first name: ";
     cout<<"Enter your last name: ";
-    
+    cout<<"Here's the information in a single string: ";
     return 0;
 }
