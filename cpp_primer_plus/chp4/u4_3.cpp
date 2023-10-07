edaab3845625e9c2757c49b3835adc4f735fa780
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-07 23:37:26 +0800

    vault backup: 2023-10-07 23:37:26

--- cpp_primer_plus/chp4/u4_3.cpp
+++ cpp_primer_plus/chp4/u4_3.cpp
@@ -4,9 +4,12 @@ using namespace std;
 int main(){
     char firstname[20], lastname[20], finalname[50];
     cout<<"Enter your first name: ";
-    cin>>firstname
-    strcpy(firstname, firstname )
+    cin.getline(firstname,20);
     cout<<"Enter your last name: ";
-    cout<<"Here's the information in a single string: ";
+    cin.getline(lastname,20);
+    strcpy(finalname,firstname);
+    strcat(finalname,", ");
+    strcat(finalname,lastname);
+    cout<<"Here's the information in a single string: "<<finalname<<endl;
     return 0;
 }
