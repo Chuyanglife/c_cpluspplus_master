1852fd856804fcaf9693d96af95a30803d83e057
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-07 23:48:31 +0800

    vault backup: 2023-10-07 23:48:31

--- cpp_primer_plus/chp4/u4_4.cpp
+++ cpp_primer_plus/chp4/u4_4.cpp
@@ -2,6 +2,12 @@
 #include<string>
 using namespace std;
 int main(){
-    cout<<""
+    string firstname,lastname,finalname;
+    cout<<"Enter your first name: ";
+    getline(cin,firstname);
+    cout<<"Enter your last name: ";
+    getline(cin,lastname);
+    
+    cout<<"Here's the information in a single string: ";
     return 0;
 }
