9293ccbaefe2e91f5ac05a5afa37b32da2bfeb6e
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-08 21:58:19 +0800

    vault backup: 2023-10-08 21:58:19

--- cpp_primer_plus/chp4/u4_7.cpp
+++ cpp_primer_plus/chp4/u4_7.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#
+#include<string>
 using namespace std;
 struct Pizza{
     string company;
@@ -8,6 +8,15 @@ struct Pizza{
 };
 int main(){
     Pizza pizza;
-
+    cout << "Enter the pizza company: ";
+    getline(cin,pizza.company);
+    cout << "Enter the diameter of pizza: ";
+    cin>>pizza.diameter;
+    cout << "Enter the weight of pizza: ";
+    cin>>pizza.weight;
+    cout <<endl<< "Here is the pizza information: "<<endl;
+    cout<<"Company: "<<pizza.company<<endl;
+    cout<<"Diameter: "<<pizza.diameter<<endl;
+    cout<<"Weight: "<<pizza.weight<<endl;
     return 0;
 }
