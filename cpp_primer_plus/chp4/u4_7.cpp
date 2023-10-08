afb2006753566def5a086f2a8bdb32bb4eb80ae0
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-08 21:55:15 +0800

    vault backup: 2023-10-08 21:55:15

--- cpp_primer_plus/chp4/u4_7.cpp
+++ cpp_primer_plus/chp4/u4_7.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
+#
 using namespace std;
 struct Pizza{
     string company;
     double diameter;
-    
-}
+    double weight;
+};
 int main(){
+    Pizza pizza;
+
     return 0;
 }
