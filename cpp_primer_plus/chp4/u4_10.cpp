fedc98b483343c65fa07d85eefcbe69f9dbac1e8
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-09 20:14:11 +0800

    vault backup: 2023-10-09 20:14:11

--- cpp_primer_plus/chp4/u4_10.cpp
+++ cpp_primer_plus/chp4/u4_10.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 int main(){
     array<double,3>result;
-    cout<<""
+    cout<<"Enter three result of the 40 meters running time: "<<endl;
+    cin>>result[0];
+    cin>>result[1];
+    cin>>result[2];
+    double ave_result=(result[0]+result[1]+result[2])/3;
+    cout<<"The all three time results is are: "<<result[0]<<", "<<result[1]<<", "<<result[2]<<endl;
+    cout<<"The average result is: "<<ave_result<<endl;
     return 0;
 }
