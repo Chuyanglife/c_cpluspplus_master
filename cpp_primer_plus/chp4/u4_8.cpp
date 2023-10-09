e927a61f68cc1d2f81696289a4a19c39c5265897
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-09 19:44:35 +0800

    vault backup: 2023-10-09 19:44:35

--- cpp_primer_plus/chp4/u4_8.cpp
+++ cpp_primer_plus/chp4/u4_8.cpp
@@ -16,6 +16,7 @@ int main(){
     getline(cin,pizza->company);
     cout<<endl<<"Here is the pizza information: "<<endl;
     cout<<"Company: "<<pizza->company<<endl;
-    cout<<"Diameter: "<<pizza->
+    cout<<"Diameter: "<<pizza->diameter<<endl;
+    cout<<"Weight: "<<pizza->weight<<endl;
     return 0;
 }
