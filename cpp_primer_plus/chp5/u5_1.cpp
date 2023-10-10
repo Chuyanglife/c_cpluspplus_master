65b733801a551460001bb10a9f8a545dcc8bcfbd
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-10 16:24:04 +0800

    vault backup: 2023-10-10 16:24:04

--- cpp_primer_plus/chp5/u5_1.cpp
+++ cpp_primer_plus/chp5/u5_1.cpp
@@ -8,8 +8,13 @@ int main(){
     cin>>number2;
     if(number1>number2){
         int tmp=number1;
-        number2=number1;
-        
+        number1=number2;
+        number2=tmp;
     }
+    int total_number=0;
+    for(int i=number1;i<=number2;i++){
+        total_number+=i;
+    }
+    cout<<"Sum the number from "<<number1<<" to "<<number2<<", sum = "<<total_number<<endl;
     return 0;
 }
