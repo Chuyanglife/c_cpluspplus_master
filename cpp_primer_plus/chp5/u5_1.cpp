09d0828930b18be90d6f30727f2663a798bb26cf
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-10 16:12:36 +0800

    vault backup: 2023-10-10 16:12:36

--- cpp_primer_plus/chp5/u5_1.cpp
+++ cpp_primer_plus/chp5/u5_1.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 int main(){
     int number1,number2;
-    cout<<"Enter the first number: "
+    cout<<"Enter the first number: ";
+    cin>>number1;
+    cout<<"Enter the second number: "
     return 0;
 }
