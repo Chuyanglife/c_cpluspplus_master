f2dc34df700224ba22b7e00b0722daa829ef96d7
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-10 20:42:47 +0800

    vault backup: 2023-10-10 20:42:47

--- cpp_primer_plus/chp5/u5_3.cpp
+++ cpp_primer_plus/chp5/u5_3.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 int main(){
     double s=0;
-    
+    double ch;
+    while(cin>>ch){
+        if(ch==0){
+            break;
+        }
+        s+=ch;
+    }
+    cout<<""
     return 0;
 }
