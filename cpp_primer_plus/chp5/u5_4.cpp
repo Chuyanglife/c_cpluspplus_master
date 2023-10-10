d3405328e59794417d416536afe0865b2925f00a
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-10 21:07:08 +0800

    vault backup: 2023-10-10 21:07:08

--- cpp_primer_plus/chp5/u5_4.cpp
+++ cpp_primer_plus/chp5/u5_4.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/* #include<iostream>
 using namespace std;
 int main(){
     double daphne_acc=100;
@@ -9,7 +9,30 @@ int main(){
         daphne_acc+=10;
         cleo_acc+=cleo_acc*0.05;
     }
-    cout<<"After "<<year<<" Years."<<"Cleo's accounts is "<<cleo_acc<<" while more than the one of Daphne whick is "
+    cout<<"After "<<year<<" Years."<<"Cleo's accounts is "<<cleo_acc<<" while more than the one of Daphne which is "
     <<daphne_acc<<"."<<endl;
+    return 0;
+} */
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    double daphne_account = 100;
+    double cleo_account = 100;
+
+    int year = 0;
+    while (cleo_account <= daphne_account) {
+        ++year;
+
+        daphne_account += 10;
+        cleo_account += cleo_account * 0.05;
+    }
+
+    cout << "After " << year << " Years. " 
+         << "Cleo's account is " << cleo_account
+         << " while more than the one of Daphne which is " 
+         << daphne_account << "." << endl;
+
     return 0;
 }
