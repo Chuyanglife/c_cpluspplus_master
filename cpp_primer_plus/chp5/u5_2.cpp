ddbe382cbc852d89f109a77dbe2583951cf74ede
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-10 20:37:34 +0800

    vault backup: 2023-10-10 20:37:34

--- cpp_primer_plus/chp5/u5_2.cpp
+++ cpp_primer_plus/chp5/u5_2.cpp
@@ -8,6 +8,9 @@ int main(){
     for(int i=2;i<=factorials.size();i++){
         factorials[i]=i*factorials[i-1];
     }
+    for(int i=2;i<=factorials.size();i++){
+        factorials[i]=i*factorials[i-1];
+    }
     return 0;
 }
 /* #include <iostream>
