df2f68090660eeec11aeb36fcb491d1f3fd6f60f
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-13 20:21:55 +0800

    vault backup: 2023-10-13 20:21:55

--- cpp_primer_plus/chp5/u5_5.cpp
+++ cpp_primer_plus/chp5/u5_5.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main(){
     string months[12]={"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
     int sell[12];
-    int total_sales
+    int total_sales=0;
+
+    for(int i=0;i<=12;i++){
+        cout<<""
+    }
     return 0;
 } 
