e3177214a3cccb579d154f10cdc7f8b06f5f5afa
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-13 20:21:33 +0800

    vault backup: 2023-10-13 20:21:33

--- cpp_primer_plus/chp5/u5_5.cpp
+++ cpp_primer_plus/chp5/u5_5.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(){
     string months[12]={"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
     int sell[12];
-    
+    int total_sales
     return 0;
 } 
