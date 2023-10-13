874b78b0f8dd3ff1f4eb8167a3c03f2fca31e0d5
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-13 20:23:01 +0800

    vault backup: 2023-10-13 20:23:01

--- cpp_primer_plus/chp5/u5_5.cpp
+++ cpp_primer_plus/chp5/u5_5.cpp
@@ -5,10 +5,10 @@ int main(){
     string months[12]={"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
     int sell[12];
     int total_sales=0;
-
+    cout<<"Enter the sales of book <<C++ for Fools>> each month." << endl;
     for(int i=0;i<=12;i++){
-        cout<<"Enter the sales of book <<C++ for Fools>> each month." << endl;
-        
+        cout<<months[i]=":";
+        cin>>sell[]
     }
     return 0;
 } 
