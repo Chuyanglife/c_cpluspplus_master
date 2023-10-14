79ddc63428e7404b85c06f0a6731ab3bb739c722
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-14 23:31:14 +0800

    vault backup: 2023-10-14 23:31:14

--- cpp_primer_plus/chp5/u5_6.cpp
+++ cpp_primer_plus/chp5/u5_6.cpp
@@ -1,4 +1,4 @@
-/* #include<iostream>
+#include<iostream>
 #include<string>
 using namespace std;
 int main(){
@@ -18,40 +18,5 @@ int main(){
     }
     cout<<"Three years total sales is "<<total_sales[0]+total_sales[1]+total_sales[2]<<endl;
     return 0;
-}  */
-#include <iostream>
-#include <string>
-
-
-int main() {
-    using namespace std;
-
-    string months[12] = {"Jan", "Feb", "Mar", "Apr", 
-                         "May", "Jun", "Jul", "Aug", 
-                         "Sep", "Oct", "Nov", "Dec"};
-    int sells[3][12];
-    int total_sales[3] = {0, 0, 0};
-
-    for (int i=0; i<3; ++i) {
-
-        cout << "Enter " << i+1 << " year(s) sales of book <<C++ for Fools>> each month." << endl;
-        for (int j=0; j<12; ++j) {
-            cout << months[j] << ": ";
-            cin >> sells[i][j];
-
-            total_sales[i] += sells[i][j];
-
-        }
-    }
-
-    for (int i=0; i<3; ++i) {
-        cout << i+1 << " year(s) total sales is " 
-             << total_sales[i] << endl;
-    }
-
-    cout << "There years total sales is " 
-         << total_sales[0] + total_sales[1] + total_sales[2] << endl;
-
-    return 0;
-}
+} 
 
