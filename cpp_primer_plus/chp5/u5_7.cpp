c656a5f216d6aaeacb50f796dba24299ee7c8685
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-20 22:12:16 +0800

    Update u5_7.cpp

--- cpp_primer_plus/chp5/u5_7.cpp
+++ cpp_primer_plus/chp5/u5_7.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 using namespace std;
+struct car{
+    string made;
+    int year;
+
+};
 int main(){
+    int car_num=0;
+    cout<<"How many cars do you wish to catalog?"<<endl;
+    cin>>car_num;
+    cin.get();
+    
     return 0;
 }
