efe704821beefd8f296d9437b680b0a4add8fae9
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-25 20:41:58 +0800

    vault backup: 2023-10-25 20:41:58

--- cpp_primer_plus/demo_plus/demo1.cpp
+++ cpp_primer_plus/demo_plus/demo1.cpp
@@ -12,25 +12,3 @@ int main(){
     //system("pause");
     return 0;
 }  */
-#include<iostream>
-using namespace std;
-int main(){
-    int a= INT_MAX;
-    short b= SHRT_MAX;
-    long c=LONG_MAX;
-    long long d=LLONG_MAX;
-    cout<<"int: "<<sizeof(int)<<" bytes"<<endl;
-    cout<<"short: "<<sizeof(short)<<" bytes"<<endl;
-    cout<<"long: "<<sizeof(long)<<" bytes"<<endl;
-    cout<<"long long: "<<sizeof(long long)<<" bytes"<<endl;
-    cout<<endl;
-    cout<<"int: "<<a<<endl;
-    cout<<"short: "<<b<<endl;
-    cout<<"long: "<<c<<endl;
-    cout<<"long long: "<<d<<endl;
-
-    int qq(5);
-    int q{1};
-    cout<<"qq:"<<qq<<endl<<"q:"<<q<<endl;
-    return 0;
-}
