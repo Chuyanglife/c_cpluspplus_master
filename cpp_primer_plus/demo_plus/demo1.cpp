67dc8bc298ea5c03b72bc15cfa0ae6608ab41785
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-10-26 19:47:45 +0800

    vault backup: 2023-10-26 19:47:45

--- cpp_primer_plus/demo_plus/demo1.cpp
+++ cpp_primer_plus/demo_plus/demo1.cpp
@@ -12,3 +12,18 @@ int main(){
     //system("pause");
     return 0;
 }  */
+#include<iostream>
+#define ZERO 0;
+using namespace std;
+int main(){
+    short a = SHRT_MAX;
+    unsigned short aa= a;
+    cout<<"short:"<<a<<endl;
+    cout<<"unsigned short:"<<aa<<endl;
+    a= a+1;
+    aa= aa+1;
+    cout<<"short:"<<a<<endl;
+    cout<<"unsigned short:"<<aa<<endl;
+
+    return 0;
+}
